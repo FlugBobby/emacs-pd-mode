cbe4726ec9bda5e159250b285a1d6892c9fc9286
Florent Decherf <florent.decherf@epitech.eu>
2015-04-09 12:24:54 +0100

    more keywords, more regexp

--- pd-mode.el
+++ pd-mode.el
@@ -7,7 +7,6 @@
       (comment-dwim arg)))
 
 (setq pd-keywords-lvl0 '("abstract"
-			 "assert"
 			 "class"
 			 "confined"
 			 "deferred"
@@ -19,26 +18,24 @@
 			 "invariant"
 			 "post"
 			 "pre"
-			 "property"
 			 "class" ))
 
-(setq pd-keywords-lvl1 '("build"
+(setq pd-keywords-lvl1 '("assert"
+			 "build"
+			 "const"
 			 "function"
 			 "ghost"
 			 "nonmember"
 			 "opaque"
+			 "property"
 			 "schema"
 			 "selector"
 			 "var"))
-
-
-
 
 (setq pd-keywords-lvl2 '("after"
 			 "any"
 			 "begin"
 			 "catch"
-			 "const"
 			 "decrease"
 			 "done"
 			 "exists"
@@ -87,13 +84,6 @@
 		     "null"
 		     ))
 
-
-
-;; (setq pd-keywords-mnr '("in" "for" "ghost" "those" "forall" "let" "yield" "over" "that" "try" "throw" "self" "pragma" "it" "any" "after" "catch" "goto" "import" "exists" "pass" "property" "satisfy" ))
-;; (setq pd-vars '("map" "set" "queue" "bag" "seq" "pair" "of" "enum" "triple" "as"))
-;; (setq pd-functions '("function" "schema" "build" "var"))
-;; (setq pd-constants '("true" "false" "null"))
-
 (setq pd-keywords-lvl0-regexp	(regexp-opt pd-keywords-lvl0 'words))
 (setq pd-keywords-lvl1-regexp	(regexp-opt pd-keywords-lvl1 'words))
 (setq pd-keywords-lvl2-regexp	(regexp-opt pd-keywords-lvl2 'words))
@@ -102,41 +92,38 @@
 
 (setq pd-function-name-regexp	"function\s*\\([^ ]+?\\)\s*[\(;:]")
 (setq pd-function2-name-regexp	"function\s*\!?\s*\\([^ ]+?\\)\s*:")
-(setq pd-class-name-regexp	"class\s*\\([^ ]+?\\)\s*.=")
+(setq pd-class-name-regexp	"class\s*\\([^ ]+?\\)\s*^=")
 (setq pd-schema-name-regexp	"schema\s*\\([^ ]+?\\)\s*[\(:]")
+
+(setq pd-of-types-name-regexp	"of\s*\\([^ \(\)]+?\\)\s*\n")
+(setq pd-colon-types-name-regexp	"\s*[^\]:]\s*:\s*\\([^: ]+?\\)\s*[;,\)}\s*\n]")
+(setq pd-vars-name-regexp	"[\(\{\,]\s*\\([^ ]+?\\)\s*:")
+(setq pd-var-vars-name-regexp	"var\s*\\([^ ]+?\\)\s*:")
+(setq pd-const-vars-name-regexp	"const\s*\\([^ ]+?\\)\s*^=")
+
+(setq pd-operator-regexp	"operator[\+-\\*%]\s*")
 
 
 (setq pd-font-lock-keywords
       `(
-	 ;; ("class\s*\\([^ ]+?\\)\s*.=" . ( 1 font-lock-comment-face))
-	;; ("function\s*\\([^ ]+?\\)\s*\(" . ( 1 font-lock-comment-face))
-	;; ("function\s*\\([^ ]+?\\)\s*;" . ( 1 font-lock-comment-face))
-	;; ("function\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
-	;; ("schema\s*\\([^ ]+?\\)\s*[\(:]" . ( 1 font-lock-comment-face))
-	;; ("function\s*\!\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
-
 	(,pd-function-name-regexp	. (1 font-lock-variable-name-face))
 	(,pd-function2-name-regexp	. (1 font-lock-variable-name-face))
 	(,pd-class-name-regexp		. (1 font-lock-variable-name-face))
 	(,pd-schema-name-regexp		. (1 font-lock-variable-name-face))
 
-	;; ("\s*[^\]:]\s*:\s*\\([^: ]+?\\)\s*[;,\)}\s*\n]" . ( 1 font-lock-string-face))
-	;; ("of\s*\\([^ \(\)]+?\\)\s*\n" . ( 1 font-lock-string-face))
+	(,pd-of-types-name-regexp	. (1 font-lock-string-face))
+	(,pd-colon-types-name-regexp	. (1 font-lock-string-face))
+	(,pd-vars-name-regexp		. (1 font-lock-variable-name-face))
+	(,pd-var-vars-name-regexp	. (1 font-lock-variable-name-face))
+	(,pd-const-vars-name-regexp	. (1 font-lock-variable-name-face))
 
-	;; ("(\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
-	;; ("{\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
-	;; ("var\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
-	;; (",\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
+	(,pd-operator-regexp		. (1 font-lock-function-name-face))
 
-	;; ("operator[\+-\\*%]"	 . font-lock-function-name-face)
-
-
-	;; (,pd-constants-regexp	 . font-lock-function-name-face) 
-	(,pd-keywords-lvl0-regexp . font-lock-keyword-face)
-	(,pd-keywords-lvl1-regexp . font-lock-function-name-face)
-	(,pd-keywords-lvl2-regexp . font-lock-keyword-face)
-	(,pd-constants-regexp	 . font-lock-constant-face)
-	(,pd-types-regexp	 . font-lock-string-face)
+	(,pd-keywords-lvl0-regexp	. font-lock-keyword-face)
+	(,pd-keywords-lvl1-regexp	. font-lock-function-name-face)
+	(,pd-keywords-lvl2-regexp	. font-lock-keyword-face)
+	(,pd-constants-regexp		. font-lock-constant-face)
+	(,pd-types-regexp		. font-lock-string-face)
 	)
       )
 
@@ -147,7 +134,7 @@
 	(modify-syntax-entry ?\n "> b" synTable)
 	     synTable))
 
-(define-derived-mode pd-mode fundamental-mode
+(define-derived-mode pd-mode prog-mode
   "pd-mode is a major mode for editing language Perfect developer."
   :syntax-table pd-syntax-table
   (setq font-lock-defaults '((pd-font-lock-keywords)))
