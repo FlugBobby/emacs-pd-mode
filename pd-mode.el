956645a1daea9c25a3697c3eb2957524ea6d2817
Florent Decherf <florent.decherf@epitech.eu>
2015-04-09 12:16:44 +0100

    more keywords, more regexp

--- pd-mode.el
+++ pd-mode.el
@@ -6,53 +6,137 @@
 	  )
       (comment-dwim arg)))
 
+(setq pd-keywords-lvl0 '("abstract"
+			 "assert"
+			 "class"
+			 "confined"
+			 "deferred"
+			 "end"
+			 "final"
+			 "import"
+			 "interface"
+			 "internal"
+			 "invariant"
+			 "post"
+			 "pre"
+			 "property"
+			 "class" ))
+
+(setq pd-keywords-lvl1 '("build"
+			 "function"
+			 "ghost"
+			 "nonmember"
+			 "opaque"
+			 "schema"
+			 "selector"
+			 "var"))
 
 
 
-(setq pd-keywords-mjr '("class" "end" "post" "pre" "invariant" "interface" "abstract" "assert" "absurd" "axiom" "ghost"))
-(setq pd-keywords-mnr '("in" "for" "those" "forall" "let" "yield" "over" "that" "try" "throw" "self" "pragma" "it" "any" "after" "catch" "goto" "import" "exists" "pass" "property" "satisfy" ))
-(setq pd-vars '("map" "set" "queue" "bag" "seq" "pair" "of" "enum" "triple" "as"))
-(setq pd-functions '("function" "schema" "build" "var"))
-(setq pd-constants '("true" "false" "null"))
 
-(setq pd-keywords-mjr-regexp	(regexp-opt pd-keywords-mjr 'words))
-(setq pd-keywords-mnr-regexp	(regexp-opt pd-keywords-mnr 'words))
-(setq pd-vars-regexp		(regexp-opt pd-vars 'words))
-(setq pd-functions-regexp	(regexp-opt pd-functions 'words))
+(setq pd-keywords-lvl2 '("after"
+			 "any"
+			 "begin"
+			 "catch"
+			 "const"
+			 "decrease"
+			 "done"
+			 "exists"
+			 "fi"
+			 "for"
+			 "forall"
+			 "goto"
+			 "has"
+			 "if"
+			 "in"
+			 "inherits"
+			 "keep"
+			 "let"
+			 "like"
+			 "loop"
+			 "over"
+			 "par"
+			 "pass"
+			 "proof"
+			 "rank"
+			 "satisfy"
+			 "self"
+			 "storable"
+			 "that"
+			 "then"
+			 "those"
+			 "throw"
+			 "try"
+			 "value"
+			 "via"
+			 "within"
+			 "yield"))
+
+(setq pd-types '("as"
+		 "bag"
+		 "enum"
+		 "map"
+		 "of"
+		 "pair"
+		 "seq"
+		 "set"
+		 "triple"))
+
+(setq pd-constants '("true"
+		     "false"
+		     "null"
+		     ))
+
+
+
+;; (setq pd-keywords-mnr '("in" "for" "ghost" "those" "forall" "let" "yield" "over" "that" "try" "throw" "self" "pragma" "it" "any" "after" "catch" "goto" "import" "exists" "pass" "property" "satisfy" ))
+;; (setq pd-vars '("map" "set" "queue" "bag" "seq" "pair" "of" "enum" "triple" "as"))
+;; (setq pd-functions '("function" "schema" "build" "var"))
+;; (setq pd-constants '("true" "false" "null"))
+
+(setq pd-keywords-lvl0-regexp	(regexp-opt pd-keywords-lvl0 'words))
+(setq pd-keywords-lvl1-regexp	(regexp-opt pd-keywords-lvl1 'words))
+(setq pd-keywords-lvl2-regexp	(regexp-opt pd-keywords-lvl2 'words))
+(setq pd-types-regexp		(regexp-opt pd-types 'words))
 (setq pd-constants-regexp	(regexp-opt pd-constants 'words))
+
+(setq pd-function-name-regexp	"function\s*\\([^ ]+?\\)\s*[\(;:]")
+(setq pd-function2-name-regexp	"function\s*\!?\s*\\([^ ]+?\\)\s*:")
+(setq pd-class-name-regexp	"class\s*\\([^ ]+?\\)\s*.=")
+(setq pd-schema-name-regexp	"schema\s*\\([^ ]+?\\)\s*[\(:]")
 
 
 (setq pd-font-lock-keywords
       `(
-	("class\s\\([^ ]+?\\)\s.=" . ( 1 font-lock-comment-face)) 
-	("function\s\\([^ ]+?\\)\s*\(" . ( 1 font-lock-comment-face))
-	("function\s*\\([^ ]+?\\)\s*;" . ( 1 font-lock-comment-face))
-	("function\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face)) 
-	("schema\s\\([^ ]+?\\)\s*\(" . ( 1 font-lock-comment-face)) 
-	("schema\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face)) 
-	("function\s*\!\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face)) 
+	 ;; ("class\s*\\([^ ]+?\\)\s*.=" . ( 1 font-lock-comment-face))
+	;; ("function\s*\\([^ ]+?\\)\s*\(" . ( 1 font-lock-comment-face))
+	;; ("function\s*\\([^ ]+?\\)\s*;" . ( 1 font-lock-comment-face))
+	;; ("function\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
+	;; ("schema\s*\\([^ ]+?\\)\s*[\(:]" . ( 1 font-lock-comment-face))
+	;; ("function\s*\!\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
 
-	("[^\]:]:\s*\\([^: ]+?\\)\s*;" . ( 1 font-lock-string-face)) 
-	("[^\]:]:\s*\\([^: ]+?\\)\s*," . ( 1 font-lock-string-face)) 
-	("[^\]:]:\s*\\([^: ]+?\\)\s*\)" . ( 1 font-lock-string-face)) 
-	("[^\]:]:\s*\\([^: ]+?\\)\s*}" . ( 1 font-lock-string-face)) 
-	("[^\]:]:\s*\\([^: ]+?\\)\s*\n" . ( 1 font-lock-string-face))
-	("of\s*\\([^ \(\)]+?\\)\s*\n" . ( 1 font-lock-string-face))
+	(,pd-function-name-regexp	. (1 font-lock-variable-name-face))
+	(,pd-function2-name-regexp	. (1 font-lock-variable-name-face))
+	(,pd-class-name-regexp		. (1 font-lock-variable-name-face))
+	(,pd-schema-name-regexp		. (1 font-lock-variable-name-face))
 
-	("(\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
-	("{\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
-	("var\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
-	(",\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
+	;; ("\s*[^\]:]\s*:\s*\\([^: ]+?\\)\s*[;,\)}\s*\n]" . ( 1 font-lock-string-face))
+	;; ("of\s*\\([^ \(\)]+?\\)\s*\n" . ( 1 font-lock-string-face))
 
-	;; ("\s*\\([^ ]+?\\)\@" . ( 1 font-lock-constant-face))
+	;; ("(\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
+	;; ("{\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
+	;; ("var\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
+	;; (",\s*\\([^ ]+?\\)\s*:" . ( 1 font-lock-comment-face))
 
-	("operator[\+-\\*%]"	 . font-lock-function-name-face)
+	;; ("operator[\+-\\*%]"	 . font-lock-function-name-face)
 
-	(,pd-keywords-mjr-regexp . font-lock-keyword-face)
-	(,pd-keywords-mnr-regexp . font-lock-keyword-face)
+
+	;; (,pd-constants-regexp	 . font-lock-function-name-face) 
+	(,pd-keywords-lvl0-regexp . font-lock-keyword-face)
+	(,pd-keywords-lvl1-regexp . font-lock-function-name-face)
+	(,pd-keywords-lvl2-regexp . font-lock-keyword-face)
 	(,pd-constants-regexp	 . font-lock-constant-face)
-	(,pd-vars-regexp	 . font-lock-string-face)
-	(,pd-functions-regexp	 . font-lock-function-name-face)
+	(,pd-types-regexp	 . font-lock-string-face)
 	)
       )
 
